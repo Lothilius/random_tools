7171c974dd4d0e3cf926e70e5f20fd3c858a6ca9
Lothilius <martin.valenzuela@bazaarvoice.com>
2016-07-06 09:32:30 +0100

    Include Close pending in a Working state.

--- helper_scripts/helpdesk_scripts/close_pending.py
+++ helper_scripts/helpdesk_scripts/close_pending.py
@@ -1,24 +1,22 @@
 __author__ = 'Lothilius'
 
 from triage_tickets.TicketList import TicketList, Ticket
-import pandas as pd
+import sys
 
 if __name__ == '__main__':
-    # try:
-    # Get the view ID for the pending view HD
-    pending_view__id = TicketList.get_view_id('Pending')
-    print pending_view__id
-    # Retrieve the list of tickets from the view
-    ticket_list = TicketList(helpdesk_que=pending_view__id)
-    # print ticket_list[0]
-    ticket_list = TicketList.reformat_as_dataframe(ticket_list)
-    # print ticket_list
-    # ticket_list.columns
-    for each in ticket_list.WORKORDERID.tolist():
-        result = Ticket.set_status(each, status='Resolved')
-        try:
-            print result['STATUS'] + '\n'
-        except KeyError:
-            print result['status'] + '\n'
-        # print Ticket.set_status(each)
-    # except:
+    try:
+        # Retrieve the list of tickets from the view
+        ticket_list = TicketList(helpdesk_que='Pending')
+        ticket_list = TicketList.reformat_as_dataframe(ticket_list)
+
+        # Work through the list and set the status to Resolved for each ticket.
+        for each in ticket_list.WORKORDERID.tolist():
+            result = Ticket.set_status(each, status='Resolved')
+            try:
+                print result['STATUS'] + '\n'
+            except KeyError:
+                print result['status'] + '\n'
+            # print Ticket.set_status(each)
+    except:
+        error_result = "Unexpected error 1CP: %s, %s" % (sys.exc_info()[0], sys.exc_info()[1])
+        print error_result
