6d6ecb9ae5540dec111564bbe7b61e3859a472fa
Lothilius <martin.valenzuela@bazaarvoice.com>
2016-07-18 10:35:47 +0200

    Update class to include specifics for retrieving users.

--- sfdc/SFDC_Users.py
+++ sfdc/SFDC_Users.py
@@ -15,18 +15,21 @@ class SFDC_Users(object):
     def __str__(self):
         return str(self.users)
 
+    def users(self):
+        return self.users
+
     @staticmethod
     def get_user_list():
         sf = SFDC.connect_to_SFDC('prod')
-        results = sf.query_all("SELECT Id, Email, Name  FROM User Where isActive=true")
+        results = sf.query_all("SELECT Id, Email, Name  FROM User Where isActive=true AND UserType = 'Standard'")
         result_list = []
 
         for each in results['records']:
             result_list.append([each['Id'], each['Email'], each['Name']])
-        results_panda = pd.DataFrame(result_list)
+        results_panda = pd.DataFrame(result_list, columns=['Id', 'Email', 'Name'])
 
         return results_panda
 
 if __name__ == '__main__':
     the_list = SFDC_Users()
-    print the_list.users
+    print the_list.users()
