c9fff3d0a18a789403399ccf69401a63ac1efd6c
Lothilius <martin.valenzuela@bazaarvoice.com>
2016-08-12 14:09:41 -0500

    Improve for proper date handling and general improvements

--- tableau_data_publisher/data_assembler.py
+++ tableau_data_publisher/data_assembler.py
@@ -5,11 +5,11 @@ __author__ = 'Lothilius'
 # The script converts the data in to a panda Dataframe in order to analyze and maintain data structure.
 
 import pandas as pd
-import sys
+from os import environ
 from datetime import datetime
 from tableausdk import *
 from tableausdk.Extract import *
-
+import numpy as np
 
 
 # Define type maps
@@ -29,49 +29,38 @@ class TDEAssembler (object):
         :return: None
         """
         self.data_frame = data_frame
-        self.file_path = file_path
+        if file_path == '' and os.environ['MY_ENVIRONMENT'] == 'prod':
+            self.file_path = '/var/shared_folder/BizApps/Tableau_data/'
+        elif file_path == '':
+            self.file_path = '/Users/%s/Downloads/' % environ['USER']
+        else:
+            self.file_path = file_path
+
         self.time_of_extract = datetime.now()
         self.extract_name = extract_name
 
         # Check the type of data passed to the assembler
         try:
-            if type(self.data_frame) is pd.DataFrame:
+            if isinstance(self.data_frame, pd.DataFrame):
                 self.assemble_tde()
-        except:
+        except ValueError:
             error_result = "Unexpected error 1: %s, %s" % (sys.exc_info()[0], sys.exc_info()[1])
             print error_result
 
     def __str__(self):
         return self.tde_file()
 
-    def add_timestamp(self, time_of_extract):
-        """ Create a column stamping all the tickets with the date and time of the extract.
-        :return: None. Appends to the data frame a column with the time and date of the extract.
-        """
-        self.data_frame['Extract_Timestamp'] = time_of_extract
-
-    def tde_file(self):
-        """ Use this in conjuction with the data publisher in order to publish the data.
-        :return: File name and path to the tde extract file
-        """
-        # Create filename with path, name a time stamp
-        if self.extract_name == '':
-            self.extract_name = 'tableau_extract_file'
-        file_name = '%s%s_%s.tde' % (self.file_path, self.extract_name, self.time_of_extract.strftime('%Y-%m-%d_%H_%M_%S'))
-
-        return file_name
-
     def assemble_tde(self):
         """ Gather the data information and create the Tde.
         :return:
         """
         #  Create time stamp and Apply the time of extract to the data frame and the name of the file
-        self.add_timestamp(self.time_of_extract.strftime('%Y-%m-%d %H:%M:%S'))
+        self.add_timestamp(self.time_of_extract)
         file_name = self.tde_file()
         data_meta = pd.DataFrame(self.data_frame.dtypes.reset_index())
         data_meta.rename(columns={'index': 'column_name', 0: 'data_type'}, inplace=True)
 
-        # print "Creating extract:", file_name
+        print "Creating extract:", file_name
 
         ExtractAPI.initialize()
 
@@ -89,9 +78,7 @@ class TDEAssembler (object):
                         table_definition.addColumn(str(each[0]), schema_type_map[str(each[1])])
                 # Create the Table with the table definition
                 table = data_extract.addTable("Extract", table_definition)
-            else:
-                # Open an existing table to add more rows
-                table = data_extract.openTable('Extract')
+
 
             new_row = Row(table_definition)
             for i in range(self.data_frame.shape[0]):
@@ -101,30 +88,54 @@ class TDEAssembler (object):
 
             data_extract.close()
 
+    def add_timestamp(self, time_of_extract):
+        """ Create a column stamping all the tickets with the date and time of the extract.
+        :return: None. Appends to the data frame a column with the time and date of the extract.
+        """
+        self.data_frame['Extract_Timestamp'] = time_of_extract
+
 
     @staticmethod
-    def add_to_row(row_object, column_number, value, type):
+    def add_to_row(row_object, column_number, value, value_type):
         """ Convert the value and add it to the row object given.
         :param row_object: The row object of the table
         :param column_number: The column number for the value you are adding
         :param value: The value you are adding. Must be of panda dtype bytes, int64, float64, or datetime64[ns]
-        :param type: The type for the column given.
+        :param value_type: The type for the column given.
         :return:
         """
-        type = str(type)
-
-        if value is None:
+        value_type = str(value_type)
+        try:
+            if column_number != 35:
+                value = datetime.combine(value, datetime.min.time())
+                value_type = 'datetime64[ns]'
+        except:
+            try:
+                if column_number != 35:
+                    value = datetime.strptime(value, '%Y-%m-%d')
+                    value_type = 'datetime64[ns]'
+            except:
+                pass
+        if value == None or str(value) == str(np.nan):
             value = 'NA'
-        if type == 'bool':
+        if value == False or value == True:
+            value = str(value)
+        if value_type == 'bool':
             row_object.setBoolean(column_number, value)
-        elif type == 'bytes':
+        elif value_type == 'bytes':
             row_object.setInteger(column_number, value)
-        elif type == 'int64':
+        elif value_type == 'int64':
+            if value == 'NA':
+                value = 0
             row_object.setLongInteger(column_number, value)
-        elif type == 'float64':
+        elif value_type == 'float64':
+            if isinstance(value, str):
+                value = float(0.0)
             row_object.setDouble(column_number, value)
-        elif type == 'datetime64[ns]':
+        elif value_type == 'datetime64[ns]':
             # Split the datetime in to its components
+            if value == 'NA':
+                value = datetime.strptime('2000-01-01 00:00:00', '%Y-%m-%d %H:%M:%S')
             year = value.year
             month = value.month
             day = value.day
