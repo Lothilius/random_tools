2c308b29f129f6c96789cc79b50334a4cddd4a9b
Lothilius <martin.valenzuela@bazaarvoice.com>
2016-08-23 13:28:17 -0500

    Include bypass alert

--- helper_scripts/one-offs/turn_off_bypass_alert.py
+++ helper_scripts/one-offs/turn_off_bypass_alert.py
@@ -1,1 +1,16 @@
 __author__ = 'Lothilius'
+
+from send_email.OutlookConnection import OutlookConnection as outlook
+import sys
+
+
+if __name__ == '__main__':
+    try:
+        name = sys.argv[1]
+    except:
+        name = 'Someone'
+    print name
+    subject = 'SFDC - Alert - Turn off Account trigger bypass.'
+    body = 'It has been at least 24 hours since the Account trigger bypass has been turned on for %s. ' \
+           'https://na3.salesforce.com/setup/ui/listCustomSettingsData.apexp?id=aAS' % name
+    outlook.create_helpdesk_ticket(subject=subject, body=body)
