b0e5a07a159f00292e4f2a50dc02883c6daf3e22
Lothilius <martin.valenzuela@bazaarvoice.com>
2017-05-11 09:27:48 -0500

    Include some data cleansing and printing change.

--- helper_scripts/concur_scripts/deprovision_concur_users.py
+++ helper_scripts/concur_scripts/deprovision_concur_users.py
@@ -5,6 +5,7 @@ from se_helpers.actions import go_to_concur_user_page
 import pandas as pd
 import sys
 
+pd.set_option('display.width', 290)
 
 if __name__ == '__main__':
     # file_location = raw_input('Hello where is the file of users: ')
@@ -14,6 +15,8 @@ if __name__ == '__main__':
     browser = go_to_concur_user_page()
 
     employee_list = pd.read_csv('/Users/martin.valenzuela/Downloads/Concur_removal.csv')
+    employee_list.drop_duplicates(subset=['Employee ID'], keep='first', inplace=True)
+    # print employee_list
     for each in employee_list[['Employee ID', 'Employee Name', 'DOT']].itertuples():
         try:
             concur_employee_deprecation(browser, str(each[1]), str(each[2]), str(each[3]))
