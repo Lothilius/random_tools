f28120893fd69452bb770425040954826675b2c0
Lothilius <martin.valenzuela@bazaarvoice.com>
2017-06-28 15:52:15 -0500

    Expand the light notice offerings.
    -set colors directly (r,y,g)
    -set an error and normal mode

--- helper_scripts/notify_helpers.py
+++ helper_scripts/notify_helpers.py
@@ -29,6 +29,62 @@ def alert_the_light():
         error_result = "Unexpected error 1TL: %s, %s" % (sys.exc_info()[0], sys.exc_info()[1])
         print error_result
 
+def set_error_light():
+    try:
+        hue_ip, hue_token = auth.hue_bridge()
+        light_bridge = phue.Bridge(hue_ip, username=hue_token)
+        light_bridge.set_light(1, parameter={"effect": "none"})
+        light_bridge.set_light(1, parameter={"alert": "select"})
+        wait(.5)
+        light_bridge.set_light(1, parameter={"alert": "select"})
+        wait(.5)
+        light_bridge.set_light(1, parameter={"hue": 65280})
+        wait(.5)
+        light_bridge.set_light(1, parameter={"alert": "lselect"})
+    except:
+        error_result = "Unexpected error 1TL: %s, %s" % (sys.exc_info()[0], sys.exc_info()[1])
+        print error_result
+
+def set_red():
+    try:
+        hue_ip, hue_token = auth.hue_bridge()
+        light_bridge = phue.Bridge(hue_ip, username=hue_token)
+        light_bridge.set_light(1, parameter={"effect": "none"})
+        light_bridge.set_light(1, parameter={"hue": 0})
+    except:
+        error_result = "Unexpected error 1TL: %s, %s" % (sys.exc_info()[0], sys.exc_info()[1])
+        print error_result
+
+def set_yellow():
+    try:
+        hue_ip, hue_token = auth.hue_bridge()
+        light_bridge = phue.Bridge(hue_ip, username=hue_token)
+        light_bridge.set_light(1, parameter={"effect": "none"})
+        light_bridge.set_light(1, parameter={"xy": [.48, .46]})
+    except:
+        error_result = "Unexpected error 1TL: %s, %s" % (sys.exc_info()[0], sys.exc_info()[1])
+        print error_result
+
+def set_green():
+    try:
+        hue_ip, hue_token = auth.hue_bridge()
+        light_bridge = phue.Bridge(hue_ip, username=hue_token)
+        light_bridge.set_light(1, parameter={"effect": "none"})
+        light_bridge.set_light(1, parameter={"xy": [.21, .7]})
+    except:
+        error_result = "Unexpected error 1TL: %s, %s" % (sys.exc_info()[0], sys.exc_info()[1])
+        print error_result
+
+def set_light_to_normal():
+    try:
+        hue_ip, hue_token = auth.hue_bridge()
+        light_bridge = phue.Bridge(hue_ip, username=hue_token)
+        light_bridge.set_light(1, parameter={"effect": "none"})
+        light_bridge.set_light(1, parameter={"xy": [.33, .33]})
+    except:
+        error_result = "Unexpected error 1TL: %s, %s" % (sys.exc_info()[0], sys.exc_info()[1])
+        print error_result
+
 def flow_the_light():
     try:
         hue_ip, hue_token = auth.hue_bridge()
@@ -41,4 +97,4 @@ def flow_the_light():
         print error_result
 
 if __name__ == '__main__':
-    flow_the_light()
+    set_light_to_normal()
