dd44beadd7348697fa433b5ea719dcf0f5a00593
Lothilius <martin.valenzuela@bazaarvoice.com>
2017-06-29 07:58:33 -0500

    Remove useless code

--- send_email/OutlookConnection.py
+++ send_email/OutlookConnection.py
@@ -40,7 +40,6 @@ class OutlookConnection(object):
         if isinstance(to, list):
             self.msg['To'] = ", ".join(to)
             self.to = to
-            print to
         else:
             self.msg['To'] = to
             self.to = [to]
@@ -64,16 +63,6 @@ class OutlookConnection(object):
                 self.bcc = [bcc]
         else:
             self.bcc = [bcc]
-
-
-    # def attach_html(self):
-    #     part1 = MIMEText(self.html, 'plain', "utf-8")
-    #     part2 = MIMEText(self.html, 'html', "utf-8")
-    #     print part1
-    #     print part2
-    #     # the HTML message, is best and preferred.
-    #     self.msg.attach(part1)
-    #     self.msg.attach(part2)
 
     def attach_file(self):
         """ Attach any files in a list or string passed to the initiation of the object.
