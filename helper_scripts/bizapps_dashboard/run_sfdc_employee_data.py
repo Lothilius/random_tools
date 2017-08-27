eb195514d1591098c5ed654ff2838a4caa5bd3e4
Lothilius <martin.valenzuela@bazaarvoice.com>
2017-08-27 00:20:24 -0500

    Add new Script for checking SFDC user permissions

--- helper_scripts/bizapps_dashboard/run_sfdc_employee_data.py
+++ helper_scripts/bizapps_dashboard/run_sfdc_employee_data.py
@@ -9,37 +9,37 @@ from tableau_data_publisher.data_assembler import TDEAssembler
 from tableau_data_publisher.data_publisher import publish_data
 from triage_tickets.TicketList import TicketList
 from helper_scripts.notify_helpers import Notifier
-give_notice = Notifier()
-
+from sfdc.SFDC_Users import SFDC_Users
+from os import remove
+from os.path import basename
 
 
 try:
     # Get tickets from the HDT view
-    tickets = TicketList(helpdesk_que='YtoD-EUS', with_resolution=True)
-    tickets = tickets.reformat_as_dataframe(tickets)
-    try:
-        tickets.drop('ATTACHMENTS', axis=1, inplace=True)
-    except:
-        print 'No Attachments column.'
+    the_list = SFDC_Users(include_licenses=True, include_permissions=True)
+    tickets = the_list.users_with_licenses_permissions()
+    # print tickets
 
     # Package in to a tde file
-    data_file = TDEAssembler(data_frame=tickets, file_path='/var/shared_folder/EUS/Tableau_data/',
-                             extract_name='EUS_HDT')
+    data_file = TDEAssembler(data_frame=tickets, extract_name='SFDC_User_data')
     # Set values for publishing the data.
     file_name = str(data_file)
     server_url, username, password, site_id, data_source_name, project = \
-        auth.tableau_publishing(datasource_type='EUS', data_source_name='EUS-Helpdesk-Tickets')
+        auth.tableau_publishing(datasource_type='BizApps', data_source_name='SFDC_User_Permissions')
 
     publish_data(server_url, username, password, site_id, file_name, data_source_name, project, replace_data=True)
     outlook().send_email(to='martin.valenzuela@bazaarvoice.com',
-                       subject='EUS-HDT-Data update complete', body='EUS-HDT-Data update complete')
+                       subject='SFDC_User_data compiling complete', body='SFDC_User_data compiling complete')
+
+    remove(file_name)
 
 except:
     error_result = "Unexpected AttributeError: %s, %s"\
                    % (sys.exc_info()[0], sys.exc_info()[1])
-    subject = 'Error with Tableau refresh script'
+    subject = 'Error with Tableau refresh script, %s' % basename(__file__)
     print error_result
     outlook().send_email('helpdesk@bazaarvoice.com', cc='martin.valenzuela@bazaarvoice.com', subject=subject, body=error_result)
-    # give_notice.set_red()
-    # give_notice.wait(120)
-    # give_notice.flow_the_light()
+    give_notice = Notifier()
+    give_notice.set_red()
+    give_notice.wait(120)
+    give_notice.flow_the_light()
