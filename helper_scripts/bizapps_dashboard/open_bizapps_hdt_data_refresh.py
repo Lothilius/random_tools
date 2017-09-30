cd0e7f92ef01134f43b26f4741bfc30cabd2f6c8
Lothilius <martin.valenzuela@bazaarvoice.com>
2017-09-29 22:23:18 -0500

    Include minor update to Open HDT script

--- helper_scripts/bizapps_dashboard/open_bizapps_hdt_data_refresh.py
+++ helper_scripts/bizapps_dashboard/open_bizapps_hdt_data_refresh.py
@@ -12,6 +12,7 @@ from helper_scripts.notify_helpers import Notifier
 from time import time
 from os import remove
 from os.path import basename
+from datetime import datetime
 
 
 give_notice = Notifier()
@@ -27,6 +28,7 @@ def backlog_levels(backlog_number):
     else:
         raise ValueError
 
+
 def main():
     try:
 
@@ -38,8 +40,6 @@ def main():
         except:
             print 'No Attachments column.'
 
-        print tickets
-        print len(tickets)
         backlog_levels(len(tickets))
         # Package in to a tde file
         data_file = TDEAssembler(data_frame=tickets, extract_name='BizApps_Open_HDT')
@@ -50,7 +50,7 @@ def main():
 
         publish_data(server_url, username, password, site_id, file_name, data_source_name, project, replace_data=True)
         outlook().send_email(to='martin.valenzuela@bazaarvoice.com',
-                           subject='HDT-Hourly update complete', body='HDT-Hourly update complete')
+                             subject='HDT-Hourly update complete', body='HDT-Hourly update complete')
         remove(file_name)
 
     except ValueError:
@@ -58,9 +58,9 @@ def main():
                        % (sys.exc_info()[0], sys.exc_info()[1])
         subject = 'Error with Hourly Tableau refresh script, %s' % basename(__file__)
         print error_result
-        # outlook().send_email(to='helpdesk@bazaarvoice.com', cc='martin.valenzuela@bazaarvoice.com', subject=subject, body=error_result)
+        outlook().send_email(to='helpdesk@bazaarvoice.com', cc='martin.valenzuela@bazaarvoice.com', subject=subject, body=error_result)
         give_notice.set_red()
-        give_notice.wait(120)
+        give_notice.wait(30)
         give_notice.flow_the_light()
 
 if __name__ == '__main__':
@@ -68,3 +68,4 @@ if __name__ == '__main__':
     main()
     end = time()
     print (end - start)/60
+    print datetime.now()
