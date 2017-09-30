26b755b0322f532c8195ec68ecfad6c20ca61bd5
Lothilius <martin.valenzuela@bazaarvoice.com>
2017-09-29 22:24:03 -0500

    Change SOQL to include all user and IsActive as a column

--- sfdc/SFDC_Users.py
+++ sfdc/SFDC_Users.py
@@ -36,8 +36,7 @@ class SFDC_Users(object):
         results = sf.query_all("SELECT Id, Username, Email, Name, Employee_ID__c, Role__c, Profile.Name, ForecastEnabled, "
                                "UserPermissionsKnowledgeUser, UserPermissionsLiveAgentUser, "
                                "UserPermissionsMarketingUser, UserPermissionsSFContentUser, "
-                               "UserPermissionsSupportUser  FROM User Where isActive=true "
-                               "AND UserType = 'Standard'")
+                               "UserPermissionsSupportUser, isActive  FROM User Where UserType = 'Standard'")
         results_panda = self.flaten_dictionary(results_od=results['records'])
         results_panda.rename(columns={'Id': 'UserId', 'Name': 'Profile_Name', 'Email': 'true_email',
                                       'Username': 'Email', }, inplace=True)
