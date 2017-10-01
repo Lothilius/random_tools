afa93c5c3e1cb47f0a61227981643a8731974ece
Lothilius <martin.valenzuela@bazaarvoice.com>
2017-10-01 18:01:57 -0500

    Include final SFDC report script
    Ready for deployment.

--- helper_scripts/bizapps_dashboard/run_sfdc_employee_data.py
+++ helper_scripts/bizapps_dashboard/run_sfdc_employee_data.py
@@ -7,39 +7,52 @@ from bv_authenticate.Authentication import Authentication as auth
 from send_email.OutlookConnection import OutlookConnection as outlook
 from tableau_data_publisher.data_assembler import TDEAssembler
 from tableau_data_publisher.data_publisher import publish_data
-from triage_tickets.TicketList import TicketList
 from helper_scripts.notify_helpers import Notifier
 from sfdc.SFDC_Users import SFDC_Users
 from os import remove
 from os.path import basename
+from datetime import datetime
+from time import time
 
 
-try:
-    # Get tickets from the HDT view
-    the_list = SFDC_Users(include_licenses=True, include_permissions=True)
-    tickets = the_list.users_with_licenses_permissions()
-    # print tickets
 
-    # Package in to a tde file
-    data_file = TDEAssembler(data_frame=tickets, extract_name='SFDC_User_data')
-    # Set values for publishing the data.
-    file_name = str(data_file)
-    server_url, username, password, site_id, data_source_name, project = \
-        auth.tableau_publishing(datasource_type='BizApps', data_source_name='SFDC_User_Permissions')
+def main():
+    try:
+        # Get SFDC_users from the Salesforcewith permissions and licenses
+        the_list = SFDC_Users(include_licenses=True, include_permissions=True)
+        sfdc_users = the_list.users_with_licenses_permissions()
 
-    publish_data(server_url, username, password, site_id, file_name, data_source_name, project, replace_data=True)
-    outlook().send_email(to='martin.valenzuela@bazaarvoice.com',
-                       subject='SFDC_User_data compiling complete', body='SFDC_User_data compiling complete')
+        # Package in to a tde file
+        data_file = TDEAssembler(data_frame=sfdc_users, extract_name='SFDC_User_data')
+        # Set values for publishing the data.
+        file_name = str(data_file)
+        server_url, username, password, site_id, data_source_name, project = \
+            auth.tableau_publishing(datasource_type='BizApps', data_source_name='SFDC_User_Permissions')
 
-    remove(file_name)
+        publish_data(server_url, username, password, site_id, file_name, data_source_name, project, replace_data=True)
+        outlook().send_email(to='martin.valenzuela@bazaarvoice.com',
+                           subject='SFDC_User_data compiling complete', body='SFDC_User_data compiling complete')
 
-except:
-    error_result = "Unexpected AttributeError: %s, %s"\
-                   % (sys.exc_info()[0], sys.exc_info()[1])
-    subject = 'Error with Tableau refresh script, %s' % basename(__file__)
-    print error_result
-    outlook().send_email('helpdesk@bazaarvoice.com', cc='martin.valenzuela@bazaarvoice.com', subject=subject, body=error_result)
-    give_notice = Notifier()
-    give_notice.set_red()
-    give_notice.wait(120)
-    give_notice.flow_the_light()
+        remove(file_name)
+
+    except KeyboardInterrupt:
+        pass
+    except:
+        error_result = "Unexpected AttributeError: %s, %s"\
+                       % (sys.exc_info()[0], sys.exc_info()[1])
+        subject = 'Error with Tableau refresh script, %s' % basename(__file__)
+        print error_result
+        outlook().send_email('helpdesk@bazaarvoice.com', cc='martin.valenzuela@bazaarvoice.com', subject=subject, body=error_result)
+        give_notice = Notifier()
+        give_notice.set_red()
+        give_notice.wait(30)
+        give_notice.flow_the_light()
+
+
+if __name__ == '__main__':
+    start = time()
+    main()
+    end = time()
+    print (end - start) / 60
+    print datetime.now()
+    print '-----------------'
