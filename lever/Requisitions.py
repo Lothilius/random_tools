76bf7ea3c5361280c3e73fc2bc3bdfecabc24d54
Lothilius <martin.valenzuela@bazaarvoice.com>
2017-11-20 18:09:52 -0600

    Include functioning Requisition class

--- lever/Requisitions.py
+++ lever/Requisitions.py
@@ -7,9 +7,7 @@ import sys
 
 import pandas as pd
 from pyprogressbar import Bar
-
-from LeverConnection import HelpdeskConnection as hdc
-from Ticket import Ticket
+from Lever_Connection import LeverConnection as lhc
 from helper_scripts.misc_helpers.data_manipulation import correct_date_dtype
 from time import time
 
@@ -17,59 +15,25 @@ from time import time
 pd.set_option('display.width', 340)
 pd.set_option('display.max_columns', 50)
 
-class TicketList(object):
+class Requisitions(object):
     """ The ticket list class creates an object that gathers individual tickets that belong to a particular list view.
         The list view will need to be specified from the list of view available to the person running the quarry to
         gather the tickets.
     """
-    def __init__(self, helpdesk_que='Triage', with_resolution=False, with_conversations=False, last_id=0):
-        self.ticket_cursor = 1
-        self.last_ticket_id = last_id
-        self.with_resolution = with_resolution
-        self.with_conversations = with_conversations
-        view_id = self.get_view_id(helpdesk_que)
-        self.tickets = list(self.get_all_tickets(view_id))
+    def __init__(self, record_id=''):
+        self.record_cursor = None
+        self.last_ticket_id = record_id
+        self.requisitions = self.get_all_requisitions(record_id)
 
 
     def __getitem__(self, item):
-        return self.tickets[item]
+        return self.requisitions[item]
 
     def __str__(self):
-        return str(self.tickets)
+        return str(self.requisitions)
 
-    @staticmethod
-    def get_filter_list():
-        """ Retrieve list request filters available.
-        :return: Dictionary of the ticket details
-        """
-        url, querystring, headers = hdc.create_api_request()
-
-        url = url + "/filters"
-
-        querystring['OPERATION_NAME'] = "GET_REQUEST_FILTERS"
-        del querystring['INPUT_DATA']
-        # print querystring
-        filter_list = hdc.fetch_from_helpdesk(url, querystring, headers)
-
-        return filter_list
-
-    @staticmethod
-    def get_view_id(view_name=''):
-        try:
-            # Get the view ID for the pending view HD
-            filters = pd.DataFrame(TicketList.get_filter_list())
-            view__id = filters[filters.VIEWNAME == view_name].VIEWID.iloc[0]
-            return view__id
-        except ValueError, e:
-            print e
-            view_name = raw_input('Please enter valid view name: ')
-            TicketList.get_view_id(view_name)
-        except:
-            error_result = "Unexpected error 1TL: %s, %s" % (sys.exc_info()[0], sys.exc_info()[1])
-            print error_result
-
-    def aggregate_tickets(self, ticket_list_a, ticket_list_b):
-        """ Join to lists of helpdesk tickets.
+    def aggregate_requisitions(self, ticket_list_a, ticket_list_b):
+        """ Join to lists of lever records.
 
         :param ticket_list_a: list
         :param ticket_list_b: list
@@ -79,55 +43,53 @@ class TicketList(object):
 
         return helpdesk_tickets
 
-    def get_100_tickets(self, helpdesk_que='7256000001531681_MyView_7256000001531679', from_value=1): # 7256000001531681_MyView_7256000001531679 7256000001516015_MyView_7256000000736233
-        """ Get helpdesk tickets for the respective que 100 at a time.
-        :return: list of dicts - helpdesk_tickets
+    def get_100_requisitions(self, offset='', record_id=''):
+        """ Get lever records up to 100 at a time.
+        :return: dict with lever requisition info {data, hasNext[, next]}
         """
-        url, querystring, headers = hdc.create_api_request(helpdesk_que, from_value)
+        url, querystring, headers = lhc.create_api_request(offset=offset, record_id=record_id)
 
-        return hdc.fetch_from_helpdesk(url, querystring, headers)
+        return lhc.fetch_from_lever(url, querystring, headers)
 
-    def get_all_tickets(self, helpdesk_que='7256000001531681_MyView_7256000001531679'):
+
+    def gather_requisitions(self, lever_record_list, lever_records):
         try:
-            # Get first 100 ticket from helpdesk
-            helpdesk_tickets = self.get_100_tickets(helpdesk_que=helpdesk_que)
+            self.record_cursor = lever_records['next']
+            lever_records = self.get_100_requisitions(offset=self.record_cursor)
+            lever_record_list = self.aggregate_requisitions(lever_record_list, lever_records['data'])
+
+            lever_record_list, next_lever_records = self.gather_requisitions(lever_record_list, lever_records)
+        except KeyError:
+            pass
+
+        return lever_record_list, lever_records
+
+
+    def get_all_requisitions(self, record_id=''):
+        try:
+            # Get first 100 ticket from lever
+            lever_records = self.get_100_requisitions(record_id=record_id)
+            # print type(lever_records['data'])
+            lever_record_list = lever_records['data']
 
             # Check if more than 100 exist and need to be aggregated.
-            if len(helpdesk_tickets) == 100:
-                # TODO - Make this a recursive method!!!
-                while len(helpdesk_tickets) % 100 == 0:
-                    self.ticket_cursor = self.ticket_cursor + 100
-                    helpdesk_tickets = self.aggregate_tickets(
-                        helpdesk_tickets, self.get_100_tickets(helpdesk_que=helpdesk_que,
-                                                               from_value=self.ticket_cursor))
-            ticket_details = []
+            if len(lever_record_list) == 100:
+                lever_record_list, lever_records = self.gather_requisitions(lever_record_list, lever_records)
+            else:
+                lever_record_list = [lever_record_list]
+
             try:
                 # Convert helpdesk ticket list to Dataframe
-                helpdesk_df = pd.DataFrame(helpdesk_tickets)
-                helpdesk_df['WORKORDERID'] = pd.to_numeric(helpdesk_df['WORKORDERID'], errors='coerce')
-
-                # Reduce ticket list to only tickets greater than the last ticket id
-                # Note: If now last ticket ID is given the last ticket ID is 0 and so all ticket detail is retrieved.
-                detail_list = helpdesk_df[helpdesk_df['WORKORDERID'] > self.last_ticket_id]
-                if self.last_ticket_id != 0:
-                    print 'Retrieving ticket detail from Work order ID: %s' % self.last_ticket_id
-                else:
-                    print 'Retrieving ticket detail.'
-
-                # Gather Ticket details for each in the summery dataframe
-                pbar = Bar(len(detail_list['WORKORDERID'].tolist()))
-                for i, each in enumerate(detail_list['WORKORDERID'].tolist()):
-                    # print i
-                    ticket = Ticket(str(each), self.with_resolution, self.with_conversations)
-                    ticket_details.append(ticket.details)
-                    pbar.passed()
+                lever_df = pd.DataFrame(lever_record_list)
+                lever_df = self.reformat_as_dataframe(lever_df)
+                # print lever_df
 
             except:
                 error_result = "Unexpected error 1TL: %s, %s" % (sys.exc_info()[0], sys.exc_info()[1])
                 print error_result
-                raise Exception(error_result + str(ticket_details))
+                raise Exception(error_result)
 
-            return ticket_details
+            return lever_df
         except EOFError:
             error_result = "Unexpected error 1TL: %s, %s" % (sys.exc_info()[0], sys.exc_info()[1])
             # TODO -Fix this issue so that error_message is populated!
@@ -171,20 +133,17 @@ class TicketList(object):
             pass
 
     @staticmethod
-    def reformat_as_dataframe(ticket_details):
+    def reformat_as_dataframe(requisition_details):
         """ Use to reformat responses to a panda data frame.
-        :param ticket_details: Should be in the form of an array of dicts ie [{1,2,...,n},{...}...,{...}]
+        :param requisition_details: Should be in the form of an array of dicts ie [{1,2,...,n},{...}...,{...}]
         :return: returns panda dataframe
         """
-        ticket_details = pd.DataFrame(list(ticket_details))
-        ticket_details = ticket_details.rename(columns={'UDF_CHAR1': 'Department_Group',
-                                                        'UDF_CHAR2': 'System',
-                                                        'UDF_CHAR11': 'System Component'})
-        ticket_details = ticket_details.applymap(TicketList.convert_time)
+        requisition_details = pd.DataFrame(requisition_details)
+        requisition_details = requisition_details.applymap(Requisitions.convert_time)
         # ticket_details = ticket_details.applymap(TicketList.reduce_to_year)
-        ticket_details = correct_date_dtype(ticket_details, date_time_format='%Y-%m-%d %H:%M:%S')
+        requisition_details = correct_date_dtype(requisition_details, date_time_format='%Y-%m-%d %H:%M:%S')
 
-        return ticket_details
+        return requisition_details
 
 
 if __name__ == '__main__':
