9b813b5c638aa41031b3b4db68be19868e2a8949
Lothilius <martin.valenzuela@bazaarvoice.com>
2018-01-05 18:17:52 -0600

    Fix Helpdesk Status class

--- triage_tickets/Helpdesk.py
+++ triage_tickets/Helpdesk.py
@@ -3,7 +3,8 @@ __author__ = 'Lothilius'
 import requests
 import json
 import sys
-import bv_authenticate.Authentication as auth
+from triage_tickets.TicketList import TicketList
+from Status import Status
 import datetime
 import pandas as pd
 import re
@@ -11,16 +12,11 @@ import re
 pd.set_option('display.width', 160)
 
 
-class Helpdesk(object):
+class Helpdesk(Status):
     """ Extend Status class for Helpdesk.
     """
-    def __init__(self):
-        self.tickets = self.get_all_tickets()
 
-    def __str__(self):
-        return str(self.tickets)
-
-    def count_unassigned(self, helpdesk_tickets):
+    def count_tickets(self, helpdesk_tickets):
         """Count total unassigned"""
         unassigned_total = 0
         for each in helpdesk_tickets:
@@ -30,42 +26,34 @@ class Helpdesk(object):
         return unassigned_total
 
 
+    def get_all_tickets(self):
+        try:
+            from_value = 1
+            # Get first 100 ticket from helpdesk
+            tickets = TicketList(helpdesk_que='Hourly-Open-Tickets-do-not-edit', with_detail=False)
+            self.set_status(status_value=1)
+            self.set_status_message('Functioning normally')
+            self.set_error_message('No Error Reported.')
+            tickets = TicketList.reformat_as_dataframe(tickets)
+            return len(tickets)
+        except requests.exceptions.ConnectionError:
+            error_result = "Unexpected error 1: %s" % (sys.exc_info()[0])
+            if 'Max retries exceeded with url:' in sys.exc_info()[1]:
+                self.status = 0
+                self.set_status_message('Not able to Connect to manageengine.com')
+                # TODO -Fix this issue so that error_message is populated!
+                self.error_message = error_result
+        except:
+            error_result = "Unexpected error 1: %s" % (sys.exc_info()[0])
+            self.status = 0
+            self.set_status_message('Error Loading. Possible Helpdesk outage.')
+            # TODO -Fix this issue so that error_message is populated!
+            self.error_message = error_result
 
-def convert_time(unicode_series):
-    """Given value for date time
-        Convert it to a regular datetime string"""
-    # for each in unicode_series:
-    if type(unicode_series) == int and len(unicode_series) == 10:
-        pass
-    elif type(unicode_series) == int and len(unicode_series) == 13:
-        unicode_series = unicode_series[:10]
-    try:
-        date_time_value = datetime.datetime.fromtimestamp(int(unicode_series)).strftime('%Y-%m-%d %H:%M:%S')
-        if int(date_time_value[:4]) > 2009:
-            return date_time_value
-        else:
-            return unicode_series
-    except:
-        return unicode_series
+            return -1
 
-def reduce_to_year(unicode_series):
-    try:
-        pattern = re.compile("(\d{4}-\d{2}-\d{2}\s\d{2}:\d{2}:\d{2})$")
-        match = pattern.match(unicode_series)
-        if match:
-            return unicode_series[:10]
-        else:
-            return unicode_series
-    except:
-        pass
 
-def reformat_hd_dataframe(ticket_details):
-    ticket_details = ticket_details.rename(columns={'UDF_CHAR1':'Department_Group'})
-    ticket_details = ticket_details.applymap(convert_time)
-    ticket_details = ticket_details.applymap(reduce_to_year)
-
-    return ticket_details
 
 if __name__ == '__main__':
     status = Helpdesk()
-    tickets = pd.DataFrame(status.tickets)
+    # tickets = pd.DataFrame(status.tickets)
