669991fe0f05c6cb04b0267c1038871a82766b31
Lothilius <martin.valenzuela@bazaarvoice.com>
2018-01-05 18:20:10 -0600

    Update SFDC Status class

--- sfdc/SFDC.py
+++ sfdc/SFDC.py
@@ -6,70 +6,98 @@ from bv_authenticate.Authentication import Authentication as auth
 import sys
 from bv_status.Status import Status
 from simple_salesforce import Salesforce
+import simple_salesforce.login
+import time
+import sys
 
+
+def wait(seconds=5):
+    time.sleep(seconds)
 
 class SFDC(Status):
-    """ Extend Status class fot Salesforce.
+    """ Extend Status class for Helpdesk.
     """
 
+    @staticmethod
+    def status_api_request(instance='na3'):
+        """
+        """
+        # Main SFDC api URL
+        url = "http://api.status.salesforce.com/v1/instances/%s/status" % instance
+
+        # Header information
+        headers = {
+            "Accept": "application/json"
+        }
+        return url, headers
+
+    @staticmethod
+    def fetch_status_jason(url, headers):
+        """ Makes the actual call to the Salesforce Status API.
+        :param url: Send in the base url for the REST Call
+        :param headers: Set to accept Jason or other.
+        :return: Return a Status
+        """
+        try:
+            wait(1)
+            # Create the request and capture the response.
+            response = requests.request("GET", url, headers=headers)
+            # print response.txt
+            # Load the response to the request as a json object.
+            raw_sfdc_status_jason = json.loads(response.text)
+            status_message = raw_sfdc_status_jason['status']
+
+            if len(raw_sfdc_status_jason['Incidents']) != 0:
+                status_detail = raw_sfdc_status_jason['Incidents'][0]['id']
+                status_detail = "%s: %s" % (status_detail, raw_sfdc_status_jason['Incidents'][0]['additionalInformation'])
+            else:
+                status_detail = 'No Error Reported.'
+
+            return status_message, status_detail
+        except AttributeError:
+            error_result = "Unexpected error 3: %s, %s" % (sys.exc_info()[0], sys.exc_info()[1])
+            print error_result
+            print raw_sfdc_status_jason["operation"]
+            wait(3)
+            raw_sfdc_status_jason.fetch_from_helpdesk(url, headers)
+
     def get_status(self):
-        # Get SFDC Status from Status page.
-        sfdc = requests.get("http://trust.salesforce.com/trust/instances/NA3")
-        sfdc_reply = sfdc.content
-        status = "This instance is available and fully functional." in sfdc_reply
-        if status:
-            return 1
-        else:
-            return 0
+        # Get SFDC Status from Status page using headless Webkit so that javascript is rendered.
+        try:
+            url, headers = self.status_api_request()
+            status_list, status_message = self.fetch_status_jason(url=url, headers=headers)
+
+            if status_list == 'OK':
+                self.set_status_message('Functioning normally')
+                self.set_error_message('No Error Reported.')
+                return 1
+            # Detect if non Major notice is posted.
+            elif 'INFORMATIONAL' in status_list['status'] \
+                    or 'MAINTENANCE' == status_list['status'] \
+                    or 'MINOR' == status_list['status']:
+                return 2
+            else:
+                self.set_status_message(status_message)
+                return 0
+        except:
+            error_result = "Unexpected error 2: %s, %s" % (sys.exc_info()[0], sys.exc_info()[1])
+            self.set_error_message(error_result)
+            return 2
 
     def get_error(self):
         #     TODO - Parse site for the error message.
         pass
 
-    def aggregate_tickets(self, ticket_list_a, ticket_list_b):
-        """ Join to lists ot Salesforce tickets.
-
-        :param ticket_list_a: list
-        :param ticket_list_b: list
-        :return: list - helpdesk_tickets
-        """
-        helpdesk_tickets = ticket_list_a + ticket_list_b
-
-        return helpdesk_tickets
-
-    @staticmethod
-    def build_query(sf_object='Case', type='01250000000Hnex', status='In Progress', sub_status='', technician=''):
-        if technician == '':
-            pass
-        else:
-            technician = " And OwnerId = '%s'" % technician
-
-        if status == '':
-            pass
-        else:
-            status = " And Status = '%s'" % status
-
-        if sub_status == '':
-            pass
-        else:
-            sub_status = " And Sub_Status__c = '%s'" % sub_status
-
-        query = "SELECT Id FROM %s " \
-                "WHERE RecordTypeId = '%s' " \
-                "%s%s%s" % (sf_object, type, status, sub_status, technician)
-
-        return query
 
     def get_count(self, result):
         return result['totalSize']
 
-    @staticmethod
-    def get_sfdc_data(environment='staging',
+    def get_cases(self, environment='staging',
                   type='01250000000Hnex',
                   status='In Progress',
                   sub_status='In Development',
                   technician=''):
-        """ Get Salesforce tickets for the respective que 100 at a time.
+        """ Get helpdesk tickets for the respective que 100 at a time.
         :return: OrderedDict that contains totalsize, done, and records. Records in turn is also given as an
                 OrderedDict with the actualrecords data.
         """
@@ -87,15 +115,18 @@ class SFDC(Status):
 
     def get_all_tickets(self):
         try:
-            cases = self.get_sfdc_data(environment='prod')
+            cases = self.get_cases(environment='prod')
             number_of_cases = self.get_count(cases)
             self.set_status(self.get_status())
-            self.set_status_message('Functioning normally')
-            self.set_error_message('No Error Reported.')
 
             return number_of_cases
+        except simple_salesforce.login.SalesforceAuthenticationFailed:
+            error_result = "Unexpected error 1a: %s, %s" % (sys.exc_info()[0], sys.exc_info()[1])
+            self.set_status_message('Fix SFDC BizApps Credentials')
+            self.set_error_message(error_result)
+            return -1
         except:
-            error_result = "Unexpected error 1: %s, %s" % (sys.exc_info()[0], sys.exc_info()[1])
+            error_result = "Unexpected error 1b: %s, %s" % (sys.exc_info()[0], sys.exc_info()[1])
             self.set_error_message(error_result)
 
             return -1
