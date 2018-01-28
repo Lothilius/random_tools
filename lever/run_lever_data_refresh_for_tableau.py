552ba18862fcbb52eba4a27fb334f24197d61671
Lothilius <martin.valenzuela@bazaarvoice.com>
2018-01-27 20:43:01 -0600

    Check point
    All listed tables from lever are retrieved and reformatted in to
    tableau extract files.

--- lever/run_lever_data_refresh_for_tableau.py
+++ lever/run_lever_data_refresh_for_tableau.py
@@ -18,228 +18,98 @@ from lever.Stages import Lever_Stages
 from lever.Requisitions import Requisitions
 from lever.Applications import Applications
 from lever.Requisition_Fields import Requisition_Fields
+from lever.Archive_Reasons import Archive_Reasons
 from os.path import basename
 from datetime import datetime
 from time import time
 
 
 def main():
-    testing_file_path = '/Users/martin.valenzuela/Box Sync/Documents/Austin Office/BizReqs/Lever_API_595101/'
     try:
+        testing_file_path = '/Users/martin.valenzuela/Box Sync/Documents/Austin Office/BizReqs/Lever_API_595101/'
+        stage_ids = ['44015e45-bbf3-447c-8517-55fe4540acdc', 'offer']
+
         # Get users from Lever
         users = Lever_Users()
         users = users.users
 
-        # Package in to a tde file
-        data_file = TDEAssembler(data_frame=users,
-                                 file_path=testing_file_path,
-                                 extract_name='Lever_Users')
-        # Set values for publishing the data.
-        file_name = str(data_file)
-        server_url, username, password, site_id, data_source_name, project = \
-            auth.tableau_publishing(datasource_type='PandT', data_source_name='Lever_Users')
-
-        publish_data(server_url, username, password, site_id, file_name, data_source_name, project, replace_data=True)
-    except:
-        error_result = "Unexpected AttributeError: %s, %s"\
-                       % (sys.exc_info()[0], sys.exc_info()[1])
-        subject = 'Error with Tableau refresh script, %s' % basename(__file__)
-        print error_result
-        # outlook().send_email('helpdesk@bazaarvoice.com', cc='BizAppsIntegrations@bazaarvoice.com', subject=subject, body=error_result)
-        give_notice = Notifier()
-        give_notice.set_red()
-        give_notice.wait(3)
-        give_notice.alert_the_light()
-        give_notice.flow_the_light()
-    wait(10)
-    try:
         # Get posts from Lever
         posts = Postings()
         postings = posts.full_postings
         final_posts = postings.copy(deep=True)
 
-        # Package in to a tde file
-        data_file = TDEAssembler(data_frame=final_posts,
-                                 file_path=testing_file_path,
-                                 extract_name='Lever_Posts')
-        # Set values for publishing the data.
-        file_name = str(data_file)
-        server_url, username, password, site_id, data_source_name, project = \
-            auth.tableau_publishing(datasource_type='PandT', data_source_name='Lever_Posts',)
-
-        publish_data(server_url, username, password, site_id, file_name, data_source_name, project, replace_data=True)
-    except:
-        error_result = "Unexpected AttributeError: %s, %s"\
-                       % (sys.exc_info()[0], sys.exc_info()[1])
-        subject = 'Error with Tableau refresh script, %s' % basename(__file__)
-        print error_result
-        # outlook().send_email('helpdesk@bazaarvoice.com', cc='BizAppsIntegrations@bazaarvoice.com', subject=subject, body=error_result)
-        give_notice = Notifier()
-        give_notice.set_red()
-        give_notice.wait(3)
-        give_notice.alert_the_light()
-        give_notice.flow_the_light()
-    wait(10)
-    try:
-        # Get posts from Lever
+        # Get requisitions from Lever
         requisitions = Requisitions()
         requisitions = requisitions.full_requisitions
 
-        # Package in to a tde file
-        data_file = TDEAssembler(data_frame=requisitions,
-                                 file_path=testing_file_path,
-                                 extract_name='Lever_Requisitions')
-        # Set values for publishing the data.
-        file_name = str(data_file)
-        server_url, username, password, site_id, data_source_name, project = \
-            auth.tableau_publishing(datasource_type='PandT', data_source_name='Lever_Requisitions')
-
-        publish_data(server_url, username, password, site_id, file_name, data_source_name, project, replace_data=True)
-    except:
-        error_result = "Unexpected AttributeError: %s, %s" \
-                       % (sys.exc_info()[0], sys.exc_info()[1])
-        subject = 'Error with Tableau refresh script, %s' % basename(__file__)
-        print error_result
-        # outlook().send_email('helpdesk@bazaarvoice.com', cc='BizAppsIntegrations@bazaarvoice.com', subject=subject, body=error_result)
-        give_notice = Notifier()
-        give_notice.set_red()
-        give_notice.wait(3)
-        give_notice.alert_the_light()
-        give_notice.flow_the_light()
-    wait(10)
-    try:
-        # Get posts from Lever
+        # Get candidates from Lever
         candidates = Candidates()
-        candidate_stages_ids = candidates.candidates[['candidate_id', 'stage']]
+        candidate_stages_ids = candidates.stages[['candidate_id', 'toStageId']]
         candidates = candidates.full_candidates
-        # Package in to a tde file
-        data_file = TDEAssembler(data_frame=candidates,
-                                 file_path=testing_file_path,
-                                 extract_name='Lever_Candidates')
-        # Set values for publishing the data.
-        file_name = str(data_file)
-        server_url, username, password, site_id, data_source_name, project = \
-            auth.tableau_publishing(datasource_type='PandT', data_source_name='Lever_Candidates')
 
-        publish_data(server_url, username, password, site_id, file_name, data_source_name, project, replace_data=True)
-    except:
-        error_result = "Unexpected AttributeError: %s, %s" \
-                       % (sys.exc_info()[0], sys.exc_info()[1])
-        subject = 'Error with Tableau refresh script, %s' % basename(__file__)
-        print error_result
-        # outlook().send_email('helpdesk@bazaarvoice.com', cc='BizAppsIntegrations@bazaarvoice.com', subject=subject, body=error_result)
-        give_notice = Notifier()
-        give_notice.set_red()
-        give_notice.wait(3)
-        give_notice.alert_the_light()
-        give_notice.flow_the_light()
-    wait(10)
-    try:
-        # Get posts from Lever
+        candidates_with_offers = candidate_stages_ids[candidate_stages_ids['toStageId'].isin(stage_ids)]['candidate_id']
+        candidates_with_offers = candidates_with_offers.copy()
+        candidates_with_offers.drop_duplicates(inplace=True)
+        candidates_with_offers = candidates_with_offers.tolist()
+
+        # Get stages from Lever
         stages = Lever_Stages()
         stages = stages.stages
 
-        # Package in to a tde file
-        data_file = TDEAssembler(data_frame=stages,
-                                 file_path=testing_file_path,
-                                 extract_name='Lever_Stages')
-        # Set values for publishing the data.
-        file_name = str(data_file)
-        server_url, username, password, site_id, data_source_name, project = \
-            auth.tableau_publishing(datasource_type='PandT', data_source_name='Lever_Stages')
-
-        publish_data(server_url, username, password, site_id, file_name, data_source_name, project, replace_data=True)
-    except:
-        error_result = "Unexpected AttributeError: %s, %s" \
-                       % (sys.exc_info()[0], sys.exc_info()[1])
-        subject = 'Error with Tableau refresh script, %s' % basename(__file__)
-        print error_result
-        # outlook().send_email('helpdesk@bazaarvoice.com', cc='BizAppsIntegrations@bazaarvoice.com', subject=subject, body=error_result)
-        give_notice = Notifier()
-        give_notice.set_red()
-        give_notice.wait(3)
-        give_notice.alert_the_light()
-        give_notice.flow_the_light()
-    wait(10)
-    try:
-        stage_ids = ['44015e45-bbf3-447c-8517-55fe4540acdc', 'offer']
-
-        candidates_with_offers = candidate_stages_ids[candidate_stages_ids['stage'].isin(stage_ids)]['candidate_id'].tolist()
-
-        # Get applications from Lever based on candidates with Stages in stage_ids
+        # Get applications from Lever based on candidates having been in stages that are in stage_ids
         applications = Applications(candidate_id=candidates_with_offers)
         applications = applications.full_application
         applications = correct_date_dtype(applications, date_time_format='%Y-%m-%d %H:%M:%S',
-                                               date_time_columns={'createdAt_application', 'createdAt__fields'})
+                                          date_time_columns={'createdAt_application', 'createdAt__fields'})
 
-        # Package in to a tde file
-        data_file = TDEAssembler(data_frame=applications,
-                                 file_path=testing_file_path,
-                                 extract_name='Lever_Applications')
-        # Set values for publishing the data.
-        file_name = str(data_file)
-        server_url, username, password, site_id, data_source_name, project = \
-            auth.tableau_publishing(datasource_type='PandT', data_source_name='Lever_Applications')
-
-        publish_data(server_url, username, password, site_id, file_name, data_source_name, project, replace_data=True)
-    except:
-        error_result = "Unexpected AttributeError: %s, %s" \
-                       % (sys.exc_info()[0], sys.exc_info()[1])
-        subject = 'Error with Tableau refresh script, %s' % basename(__file__)
-        print error_result
-        # outlook().send_email('helpdesk@bazaarvoice.com', cc='BizAppsIntegrations@bazaarvoice.com', subject=subject, body=error_result)
-        give_notice = Notifier()
-        give_notice.set_red()
-        give_notice.wait(3)
-        give_notice.alert_the_light()
-        give_notice.flow_the_light()
-    wait(10)
-    try:
-        # Get posts from Lever
-        requisition_fields = Requisition_Fields()
-        requisition_fields = requisition_fields.requisition_fields
-
-        # Package in to a tde file
-        data_file = TDEAssembler(data_frame=requisition_fields,
-                                 file_path=testing_file_path,
-                                 extract_name='Lever_Req_Fields')
-        # Set values for publishing the data.
-        file_name = str(data_file)
-        server_url, username, password, site_id, data_source_name, project = \
-            auth.tableau_publishing(datasource_type='PandT', data_source_name='Lever_Req_Fields')
-
-        publish_data(server_url, username, password, site_id, file_name, data_source_name, project, replace_data=True)
-    except:
-        error_result = "Unexpected AttributeError: %s, %s" \
-                       % (sys.exc_info()[0], sys.exc_info()[1])
-        subject = 'Error with Tableau refresh script, %s' % basename(__file__)
-        print error_result
-        # outlook().send_email('helpdesk@bazaarvoice.com', cc='BizAppsIntegrations@bazaarvoice.com', subject=subject, body=error_result)
-        give_notice = Notifier()
-        give_notice.set_red()
-        give_notice.wait(3)
-        give_notice.alert_the_light()
-        give_notice.flow_the_light()
-        wait(10)
-    try:
-        # Get applications from Lever based on candidates with Stages in stage_ids
+        # Get offers from Lever based on candidates with Stages in stage_ids
         offers = Offers(candidate_id=candidates_with_offers)
         offers = offers.full_offer
         offers = correct_date_dtype(offers, date_time_format='%Y-%m-%d %H:%M:%S',
-                                               date_time_columns={'createdAt', 'approvedAt', 'sentAt'})
+                                    date_time_columns={'createdAt', 'approvedAt', 'sentAt'})
 
-        # Package in to a tde file
-        data_file = TDEAssembler(data_frame=offers,
-                                 file_path=testing_file_path,
-                                 extract_name='Lever_Offers')
-        # Set values for publishing the data.
-        file_name = str(data_file)
-        server_url, username, password, site_id, data_source_name, project = \
-            auth.tableau_publishing(datasource_type='PandT', data_source_name='Lever_Offers')
+        # Get requisition_fields from Lever
+        requisition_fields = Requisition_Fields()
+        requisition_fields = requisition_fields.requisition_fields
 
-        publish_data(server_url, username, password, site_id, file_name, data_source_name, project, replace_data=True)
+        # Get archive_reasons from Lever
+        archive_reasons = Archive_Reasons()
+        archive_reasons = archive_reasons.archive_reasons
+
+        try:
+            # Create table array to iterate through for creation and publishing.
+            extract_name = [[users, 'Lever_Users'], [final_posts, 'Lever_Posts'], [requisitions, 'Lever_Requisitions'],
+                            [candidates, 'Lever_Candidates'], [stages, 'Lever_Stages'],
+                            [applications, 'Lever_Applications'],
+                            [requisition_fields, 'Lever_Req_Fields'], [offers, 'Lever_Offers'],
+                            [archive_reasons, 'Lever_Archieve_Reasons']]
+
+            file_names_to_publish = {}
+
+            for table in extract_name:
+                # Package in to a tde file
+                data_file = TDEAssembler(data_frame=table[0],
+                                         file_path=testing_file_path,
+                                         extract_name=table[1])
+                # Set values for publishing the data.
+                file_names_to_publish[table[1]] = str(data_file)
+
+                print table[1], "\n", str(data_file)
+        except:
+            error_result = "Error building tableau extract. Unexpected Error: %s, %s" \
+                           % (sys.exc_info()[0], sys.exc_info()[1])
+            subject = 'Error with Tableau refresh script, tableau extract, %s' % basename(__file__)
+            print error_result
+            # outlook().send_email('helpdesk@bazaarvoice.com', cc='BizAppsIntegrations@bazaarvoice.com', subject=subject, body=error_result)
+            give_notice = Notifier()
+            give_notice.set_red()
+            give_notice.wait(3)
+            give_notice.set_error_light()
+            give_notice.flow_the_light()
+
+
     except:
-        error_result = "Unexpected AttributeError: %s, %s" \
+        error_result = "Unexpected Error: %s, %s"\
                        % (sys.exc_info()[0], sys.exc_info()[1])
         subject = 'Error with Tableau refresh script, %s' % basename(__file__)
         print error_result
@@ -247,11 +117,32 @@ def main():
         give_notice = Notifier()
         give_notice.set_red()
         give_notice.wait(3)
-        give_notice.alert_the_light()
+        give_notice.set_error_light()
         give_notice.flow_the_light()
 
-    # outlook().send_email(to='BizAppsIntegrations@bazaarvoice.com',
-    #                      subject='HDT-Data update complete', body='HDT-Data update complete')
+
+
+    # try:
+    #
+    #     for table_name in extract_name:
+    #         server_url, username, password, site_id, data_source_name, project = \
+    #             auth.tableau_publishing(datasource_type='PandT', data_source_name=table_name[1])
+    #
+    #         # publish_data(server_url, username, password, site_id, file_names_to_publish[table_name[1]], data_source_name, project, replace_data=True)
+    #     # outlook().send_email(to='BizAppsIntegrations@bazaarvoice.com',
+    #     #                      subject='HDT-Data update complete', body='HDT-Data update complete')
+    #
+    # except:
+    #     error_result = "Error publishing tableau extracts. Unexpected Error: %s, %s"\
+    #                    % (sys.exc_info()[0], sys.exc_info()[1])
+    #     subject = 'Error with Tableau refresh script, %s' % basename(__file__)
+    #     print error_result
+    #     # outlook().send_email('helpdesk@bazaarvoice.com', cc='BizAppsIntegrations@bazaarvoice.com', subject=subject, body=error_result)
+    #     give_notice = Notifier()
+    #     give_notice.set_red()
+    #     give_notice.wait(3)
+    #     give_notice.set_error_light()
+    #     give_notice.flow_the_light()
 
 if __name__ == '__main__':
     start = time()
