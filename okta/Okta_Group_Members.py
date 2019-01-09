8360e4cddb9b00a55ba57b404fe4ebda1a68380a
Lothilius <martin.valenzuela@bazaarvoice.com>
2019-01-08 18:56:53 -0600

    Include mfa provisioning.

--- okta/Okta_Group_Members.py
+++ okta/Okta_Group_Members.py
@@ -4,6 +4,7 @@ import requests
 import json
 from okta.Okta_Connection import Okta_Connection as okta_connect
 from okta.Okta_Users import Okta_Users as okta_user
+from helper_scripts.misc_helpers.data_manipulation import create_feature_dataframe
 from bv_authenticate.Authentication import Authentication as auth
 from send_email.OutlookConnection import OutlookConnection as outlook
 import smtplib
@@ -15,17 +16,31 @@ class Okta_Group_Members(object):
     """ Group list from Okta. When called, list of groups are retrieved from Okta in a panda dataframe.
         """
 
-    def __init__(self, group_id='00gcy0wldkSLIQJLRDDU'):
+    def __init__(self, group_id='00gcy0wldkSLIQJLRDDU', query=''):
         """ Creation of instance of Okta users
         :param app_id: Should be in the format of 'app/<id>'
         """
-        if group_id == '':
-            raise(AttributeError('Group id needed.'))
-        self.primary_object = 'groups/' + group_id + '/users'
-        self.group_members = self.create_panda(self.primary_object)
+        if query != '' and '?' not in query:
+            query = '?q=' + query
+            self.primary_object = 'groups' + query
+            self.groups = self.create_panda(self.primary_object)
+        elif group_id != '':
+            self.primary_object = 'groups/' + group_id + '/users'
+            self.group_members = self.create_panda(self.primary_object)
 
     def __str__(self):
         return str(self.group_members)
+
+    @staticmethod
+    def get_groups(query=''):
+        if query != '':
+            query = '?q=' + query
+        user_data = Okta_Group_Members(query=query)
+
+        try:
+            return user_data.group_members['id'][0]
+        except Exception, e:
+            return e
 
     def add_user_to_group(self, user_email, user_id=''):
         """Provide the user id(s) or user email(s) to add to the group.
@@ -42,24 +57,27 @@ class Okta_Group_Members(object):
 
             add_to_group_url = self.primary_object + '/' + str(user_value)
             result = okta_connect(primary_object=add_to_group_url, limit='', filter='').fetch_from_okta(query_type='PUT')
-        except:
-            result = 'Fail - Unknown Error'
+        except Exception, e:
+            result = e
         return result
 
 
     def create_panda(self, primary_object):
         users = okta_connect(primary_object=primary_object, limit=500, filter='').fetch_from_okta()
         users_profile_list = pd.read_json(path_or_buf=json.dumps(users), encoding='str')
-        # print json.dumps([reponse_headers], indent=4, sort_keys=True)
-        self.group_members = pd.DataFrame(users_profile_list['profile'].tolist())
+        if users_profile_list.empty:
+            group_response = users_profile_list
+        else:
+            group_response = pd.DataFrame(users_profile_list[['profile', 'id']])
+            group_response = create_feature_dataframe(group_response, 'id', 'profile')
 
-        return self.group_members
+        return group_response
 
     def okta_users(self):
         return self.group_members
 
 
 if __name__ == '__main__':
-    user = Okta_Group_Members(group_id='00gcy0wldkSLIQJLRDDU')
-    data = pd.read_csv("/Users/martin.valenzuela/Downloads/mfa-users.csv")
-    print user.add_user_to_group(user_email='martin.valenzuela@bazaarvoice.com')
+    user = Okta_Group_Members(query='WD-Active')
+    # data = pd.read_csv("/Users/martin.valenzuela/Downloads/mfa-users.csv")
+    print user.group_members
--- okta/run_apply_mfa.py
+++ okta/run_apply_mfa.py
@@ -1,7 +1,6 @@
 # coding: utf-8
 __author__ = 'Lothilius'
 
-
 import sys
 
 from bv_authenticate.Authentication import Authentication as auth
@@ -10,97 +9,132 @@ from tableau_data_publisher.data_assembler import TDEAssembler
 from tableau_data_publisher.data_publisher import publish_data
 from helper_scripts.notify_helpers import Notifier
 from okta.Okta_Group_Members import Okta_Group_Members
-from os import remove
 from os.path import basename
 from time import time
 from datetime import datetime
+from helper_scripts.notify_helpers import wait
 import pandas as pd
+import os
 
-work_day_groups = {'WD-Active-CW-Agency': '00gvbnj7mxFBXWJLBWBT',
-                   'WD-Active-Employee-Expat': '00gvbnj7m7XOGGYTQIXA',
-                   'WD-Active-Employee-Flex': '00gvbnj7lgJYQBLIRPFJ',
-                   'WD-Active-Employee-Flex-Coach': '00g113s3nxaIhIxLn0i8',
-                    'WD-Active-Employee-Regular': '00gvbnj7lvHDVBBJVPAY',
-                    'WD-Active-Employee-Temp': '00gvbnj7l5BJKRDXFMGE',
-                    'WD-Active-Employee-Intern': '00gvbnj7mgAHCODJODGH',
-                   'WD-Active-CW-Consultant': '00gy9hrcgsZJOXSDQUFH',
-                   'WD-Active-CW-Independent': '00gvbnj7mlCYMECZUACB',
-                   'WD-Active-CW-Outsourced': '00gy9hrcguWNFPCLSFDD',
-                   'WD-Active-CW-VendorService': '00gvbnj7mpPXFTBEWRNH'
-                   }
+
+work_day_groups = Okta_Group_Members(query='WD-Active')
 m_f_group = '00gcy0wldkSLIQJLRDDU'
+today = datetime.today().strftime('%Y-%m-%d %H:%M:%S')
+
+
+def mfa_notify(user_info=('martin.valenzuela@bazaarvoice.com', 'Martin')):
+    try:
+        user_email = user_info[0]
+        user_first_name = user_info[1]
+
+        # Connect as Helpdesk
+        email = outlook(account='helpdesk@bazaarvoice.com')
+        instructions_path = os.path.join(os.path.dirname(__file__), 'mfa_instructions/')
+        html_file_name = instructions_path + 'instruction_email_html.txt'
+
+        with open(html_file_name, 'r') as html_file:
+            html = html_file.read()
+
+        html = html % user_first_name
+        file_names = ['%simage%03d.jpg' % (instructions_path, number) for number in range(1, 11)]
+
+        email.send_email(to=user_email, subject='Added to Multi Factor Authentication',
+                         body=html,
+                         html=html,
+                         files=file_names)
+    except Exception, e:
+        error_result = "Unexpected AttributeError: %s, %s, %s" \
+                       % (sys.exc_info()[0], sys.exc_info()[1], e)
+        subject = 'Error with MFA true up script. Not able to send Email. %s' % basename(__file__)
+        print error_result
+        outlook().create_helpdesk_ticket(cc='BizAppsIntegrations@bazaarvoice.com',
+                             subject=subject, body=error_result)
+        give_notice = Notifier()
+        give_notice.set_red()
+        give_notice.wait(30)
+        give_notice.flow_the_light()
+
 
 def main():
     try:
         all_wd_employees = []
-        for each in work_day_groups.keys():
+        for each in work_day_groups.groups['id'].tolist():
             # Get Users from Okta that are in the list of Workday groups
-            mfa_users = Okta_Group_Members(group_id=work_day_groups[each])
+            mfa_users = Okta_Group_Members(group_id=each)
             mfa_employees = mfa_users.group_members
             all_wd_employees.append(mfa_employees)
 
+        # Union all data frames
         all_wd_employees = pd.concat(all_wd_employees, ignore_index=True)
-
 
         # Get Users from Okta that are in mfa-everywhere group
         mfa_users = Okta_Group_Members(group_id=m_f_group)
         mfa_employees = mfa_users.group_members
 
         # Do an left outer join so all Workday employees and their MFA profiles are matched up
-        left_wd_join = pd.merge(left=all_wd_employees, right=mfa_employees[['email', 'login']], how='left', on='email',
-                              suffixes=('_wd', '_mfa'))
+        left_wd_join = pd.merge(left=all_wd_employees, right=mfa_employees[['email', 'login']],
+                                how='left', on='email', suffixes=('_wd', '_mfa'))
 
         # Any Null in login_mfa column are not in MFA group.
-        employees_without_mfa = left_wd_join[left_wd_join['login_mfa'].isnull()].copy()
-
+        employees_without_mfa = left_wd_join[left_wd_join['login_mfa'].isnull()].copy()  # type: pd.DataFrame
         print employees_without_mfa
-        file_name = '/Users/martin.valenzuela/Box Sync/Documents/Austin Office/Data/users_needing_mfa.csv'
-        employees_without_mfa.to_csv(file_name, index=False)
+        try:
+            # Package in to a tde file
+            data_file = TDEAssembler(data_frame=left_wd_join, extract_name='MFA_Employee_data')
+            # Set values for publishing the data.
+            file_name = str(data_file)
+            server_url, username, password, site_id, data_source_name, project = \
+                auth.tableau_publishing(datasource_type='BizApps', data_source_name='MFA_Employee_data')
+            # Publish the data
+            publish_data(server_url, username, password, site_id, file_name, data_source_name, project, replace_data=True)
+        except Exception, e:
+            error_result = "Unexpected AttributeError: %s, %s, %s" \
+                           % (sys.exc_info()[0], sys.exc_info()[1], e)
+            subject = 'Error with MFA true up script. Could not publish user list to Tableau. %s' % basename(__file__)
+            print error_result
+            outlook().send_email('helpdesk@bazaarvoice.com', cc='BizAppsIntegrations@bazaarvoice.com',
+                                 subject=subject, body=error_result)
+            give_notice = Notifier()
+            give_notice.set_red()
+            give_notice.wait(30)
+            give_notice.flow_the_light()
 
-        email = outlook()
+        # Apply mfa group to each user without mfa
+        employees_without_mfa['result'] = 'Success' # employees_without_mfa['email'].apply(mfa_users.add_user_to_group)
 
-        email.send_email(to="adrian.brown@bazaarvoice.com", cc='martin.valenzuela@bazaarvoice.com',
-                         subject='Users not in MFA',
-                         body="""Can you please verify I'm pulling the valid users? 
-                         The attached file has everyone that the script would automatically set to the MFA group. 
-                         Thank you 
-                         --Martin""",
-                         files=file_name)
+        # Reduce to Success list
+        employees_success = employees_without_mfa[
+            employees_without_mfa['result'].str.contains('Success')][['email', 'firstName']]
 
-        # employees_without_mfa['result'] = employees_without_mfa['email'].apply(user.add_user_to_group)
+        # Send Email notifiying user of the change
+        for each in employees_success.values.tolist():
+            mfa_notify(each)
+            wait(1)
 
-
-
-        # # Package in to a tde file
-        # data_file = TDEAssembler(data_frame=employees, extract_name='Workday_Employee_data')
-        # # Set values for publishing the data.
-        # file_name = str(data_file)
-        # server_url, username, password, site_id, data_source_name, project = \
-        #     auth.tableau_publishing(datasource_type='BizApps', data_source_name='Workday_Employee_data')
-        #
-        # publish_data(server_url, username, password, site_id, file_name, data_source_name, project, replace_data=True)
-        # outlook().send_email(to='BizAppsIntegrations@bazaarvoice.com',
-        #                    subject='Workday_User_data compiling complete', body='Workday_User_data compiling complete')
+        outlook().send_email(to='BizAppsIntegrations@bazaarvoice.com',
+                             subject='MFA_User_application compiling complete',
+                             body='MFA_User_application compiling complete')
 
 
     except KeyboardInterrupt:
         pass
-    except:
-        error_result = "Unexpected AttributeError: %s, %s"\
-                       % (sys.exc_info()[0], sys.exc_info()[1])
-        subject = 'Error with Tableau refresh script, %s' % basename(__file__)
+    except Exception, e:
+        error_result = "Unexpected AttributeError: %s, %s, %s" \
+                       % (sys.exc_info()[0], sys.exc_info()[1], e)
+        subject = 'Error with MFA true up script. Major error. ' \
+                  'Might have prevented mfa Provisioning. %s' % basename(__file__)
         print error_result
-        # outlook().send_email('helpdesk@bazaarvoice.com', cc='BizAppsIntegrations@bazaarvoice.com',
-        #                      subject=subject, body=error_result)
-        # give_notice = Notifier()
-        # give_notice.set_red()
-        # give_notice.wait(30)
-        # give_notice.flow_the_light()
+        outlook().send_email('helpdesk@bazaarvoice.com', cc='BizAppsIntegrations@bazaarvoice.com',
+                             subject=subject, body=error_result)
+        give_notice = Notifier()
+        give_notice.set_red()
+        give_notice.wait(30)
+        give_notice.flow_the_light()
 
 
 if __name__ == '__main__':
     start = time()
     main()
     end = time()
-    print (end - start)/60
+    print (end - start) / 60
     print datetime.now()
