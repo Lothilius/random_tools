2f4ce6efc65e8e207c85bca4d96f079073f38da4
Lothilius <martin.valenzuela@bazaarvoice.com>
2019-06-11 11:30:13 -0500

    Apply ManageEngine v3 api use and Tableau API improvements
    - Use ManageEngine v3 to gather tickets.
    - Use New Tableau api to create hyper extract files
    - Use New Tableau Server API to Publish data

--- helper_scripts/bizapps_dashboard/run_bizapps_data_refresh.py
+++ helper_scripts/bizapps_dashboard/run_bizapps_data_refresh.py
@@ -2,45 +2,80 @@
 __author__ = 'Lothilius'
 
 import sys
-
-from bv_authenticate.Authentication import Authentication as auth
 from send_email.OutlookConnection import OutlookConnection as outlook
-from tableau_data_publisher.data_assembler import TDEAssembler
-from tableau_data_publisher.data_publisher import publish_data
+from tableau_data_publisher.data_assembler_hyper import HyperAssembler
+from tableau_data_publisher.Tableau import Tableau
 from triage_tickets.TicketList import TicketList
+from triage_tickets.Ticket import Ticket
 from helper_scripts.notify_helpers import Notifier
 from os.path import basename
 from datetime import datetime
 from time import time
+from os import environ
+import socket
+import boto3
 
+
+if environ['MY_ENVIRONMENT'] == 'prod':
+    file_path = '/var/shared_folder/BizApps/Tableau_data/'
+else:
+    file_path = '/Users/%s/Downloads/' % environ['USER']
+    # file_path = 'Testing/BizApps/Tableau_data/'
+
+extract_name = 'BizApps_HDT'
+data_source_name = 'Helpdesk-Tickets'
+
+give_notice = Notifier()
 
 def main():
     try:
-        # Get tickets from the HDT view
-        tickets = TicketList(helpdesk_que='YtoD-BizApps', with_resolution=True)
+        # Get tickets for BizApps queues
+        tickets = TicketList(version=3, query=["BizApps - Triage", "BizApps - Integrations", "BizApps - Technical"],
+                             count_only=False)
         tickets = tickets.reformat_as_dataframe(tickets)
         try:
             tickets.drop('ATTACHMENTS', axis=1, inplace=True)
         except:
             print 'No Attachments column.'
 
-        # Package in to a tde file
-        data_file = TDEAssembler(data_frame=tickets, extract_name='BizApps_HDT')
+        tickets = tickets[['CATEGORY', 'COMPLETEDTIME', 'CREATEDBY', 'CREATEDTIME',
+                                         'DELETED_TIME', 'DEPARTMENT', 'DUEBYTIME', 'GROUP', 'HASATTACHMENTS',
+                                         'HASCONVERSATION', 'HASNOTES', 'ISPENDING', 'ITEM', 'LEVEL',
+                                         'LONG_REQUESTID', 'MODE', 'PRIORITY', 'REQUESTER', 'REQUESTEREMAIL',
+                                         'REQUESTTEMPLATE', 'RESOLUTION', 'RESOLUTIONLASTUPDATEDTIME', 'RESOLVER',
+                                         'RESPONDEDTIME', 'SHORTDESCRIPTION',
+                                         'SITE', 'SLA', 'STATUS', 'STOPTIMER', 'SUBCATEGORY', 'SUBJECT', 'TECHNICIAN',
+                                         'TEMPLATEID', 'TIMESPENTONREQ', 'Department_Group', 'System Component',
+                                         'System', 'WORKORDERID']].copy()
+
+
+        # Package in to a hyper file
+        data_file = HyperAssembler(data_frame=tickets, extract_name=extract_name, file_path=file_path)
         # Set values for publishing the data.
         file_name = str(data_file)
-        server_url, username, password, site_id, data_source_name, project = \
-            auth.tableau_publishing(datasource_type='BizApps', data_source_name='Helpdesk-Tickets')
+        tableau_server = Tableau(server_url='https://tableau.bazaarvoice.com/', site_id='BizTech')
+        tableau_server.publish_datasource(project='Business Applications',
+                                          file_path=file_name,
+                                          mode='Append', name=data_source_name)
 
-        publish_data(server_url, username, password, site_id, file_name, data_source_name, project, replace_data=True)
         outlook().send_email(to='BizAppsIntegrations@bazaarvoice.com',
                            subject='HDT-Data update complete', body='HDT-Data update complete')
 
     except:
         error_result = "Unexpected AttributeError: %s, %s"\
                        % (sys.exc_info()[0], sys.exc_info()[1])
-        subject = 'Error with Tableau refresh script, %s' % basename(__file__)
+        subject = 'Error with Tableau refresh script, %s on Server %s' % (basename(__file__), socket.gethostname())
+        error_result += subject
         print error_result
-        outlook().send_email('helpdesk@bazaarvoice.com', cc='BizAppsIntegrations@bazaarvoice.com', subject=subject, body=error_result)
+        try:
+            data = {'REQUESTEREMAIL': 'martin.valenzuela@bazaarvoice.com',
+                    'REQUESTER': 'Martin Valenzuela',
+                    'DESCRIPTION': error_result,
+                    'SUBJECT': subject}
+            Ticket().create_ticket(data)
+        except:
+            outlook().send_email('helpdesk@bazaarvoice.com', cc='BizAppsIntegrations@bazaarvoice.com', subject=subject,
+                                 body=error_result)
         give_notice = Notifier()
         give_notice.set_red()
         give_notice.wait(30)
